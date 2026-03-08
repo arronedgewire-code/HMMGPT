001101e87921fe9053c134fc60388366b19ce8ff
arronedgewire-code <arronflynn.edgewire@gmail.com>
2026-03-08 19:13:31 +0000

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,17 +1,28 @@
 import yfinance as yf
 import pandas as pd
+import time
+
 
 def load_data():
 
-    df = yf.download(
-        "BTC-USD",
-        interval="1h",
-        period="730d",
-        auto_adjust=True,
-        progress=False
-    )
+    for attempt in range(5):
 
-    df = df.dropna()
-    df.index = pd.to_datetime(df.index)
+        try:
 
-    return df
+            df = yf.download(
+                "BTC-USD",
+                interval="1h",
+                period="730d",
+                auto_adjust=True,
+                progress=False
+            )
+
+            if len(df) > 0:
+                df = df.dropna()
+                df.index = pd.to_datetime(df.index)
+                return df
+
+        except Exception:
+            time.sleep(5)
+
+    raise Exception("Failed to download BTC data from Yahoo Finance")
